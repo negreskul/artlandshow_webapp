93a76ed4417aed01849bbaa3997e4749a1a3cddf
Alexandra Negreskul <negreskula@gmail.com>
2019-08-29 00:45:35 +0300

    added contacts tab and colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faInstagram, faVk, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  faInstagram = faInstagram;
+  faVk = faVk;
+  faFacebook = faFacebook;
+  faYoutube = faYoutube;
+  
   title = 'artlandshow-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MatTabsModule } from '@angular/material';
+import { MatTabsModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './tabs/main/main.component';
@@ -11,6 +12,8 @@ import { ProjectsComponent } from './tabs/projects/projects.component';
 import { ServicesComponent } from './tabs/services/services.component';
 import { TeamComponent } from './tabs/team/team.component';
 import { ContactsComponent } from './tabs/contacts/contacts.component';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +29,11 @@ import { ContactsComponent } from './tabs/contacts/contacts.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FontAwesomeModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
